81160cfe16e7f5a66941d8461686d08e1a10e9a9
RonnyU <ronnymagdiel@gmail.com>
2021-08-01 19:51:00 -0600

    change domain and port

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 //habilitar express.json
 app.use(express.json({ extended: true }));
 
-const PORT = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 //import routes
 app.use('/api/user', require('./routes/user'));
@@ -19,7 +19,7 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/project', require('./routes/project'));
 app.use('/api/task', require('./routes/task'));
 
-app.listen(PORT, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(
     `El servidor esta corriendo en el puerto http://localhost:${PORT}/`
   );
